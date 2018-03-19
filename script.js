1dc90b63457d4a47822a80ddd92810058c1fd292
[jawdinmorris] <[jordanandrewmorris1@gmail.com]>
2018-03-19 19:55:22 +1000

    added smoothscrolling, fixed navhash

--- script.js
+++ script.js
@@ -113,3 +113,19 @@ window.onload = function() {
             }
         );
     });
+
+
+// SMOOTH SCROLLING
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing');
+	});
+});
+//REMOVE URL ON SCROLLING$('html, body').stop().animate({
